af0e5f0ea8f7aed69a033e7276876e9fe5cc0b31
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-02-26 13:33:04 -0300

    URI1004

---
+++ Seção 03 - Estrutura sequencial/24. Exercícios propostos - PARTE 1/uri1004/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int prod = a * b;
+        System.out.println("PROD = " + prod);
+
+        scanner.close();
+    }
+}
