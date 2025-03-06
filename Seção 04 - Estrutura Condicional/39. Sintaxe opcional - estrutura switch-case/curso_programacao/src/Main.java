a24ec2713cd151b1f9dd96e4e20944e372c98cd4
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-06 10:18:28 -0300

    39. Sintaxe opcional - estrutura switch-case

---
+++ Seção 04 - Estrutura Condicional/39. Sintaxe opcional - estrutura switch-case/curso_programacao/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dia;
+        int x = sc.nextInt();
+        switch (x) {
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "valor invalido";
+                break;
+        }
+        System.out.println("Dia da semana: " + dia);
+        sc.close();
+    }
+}
