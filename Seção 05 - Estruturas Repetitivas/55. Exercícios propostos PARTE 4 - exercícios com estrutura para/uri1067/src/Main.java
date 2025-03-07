fbae6dc92af82ca0232483d59538eeea67b44efc
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-07 16:08:05 -0300

    URI1067

---
+++ Seção 05 - Estruturas Repetitivas/55. Exercícios propostos PARTE 4 - exercícios com estrutura para/uri1067/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        for (int i = 0; i <= x; i++){
+            if (i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
+}
