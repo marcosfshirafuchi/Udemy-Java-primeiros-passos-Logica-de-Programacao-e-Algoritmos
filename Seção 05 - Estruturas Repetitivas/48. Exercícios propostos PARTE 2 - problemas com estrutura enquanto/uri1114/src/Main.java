b54952c177783b48c1b118725a26516e7069ffeb
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-03-07 12:21:27 -0300

    48. Exercícios propostos PARTE 2 - problemas com estrutura enquanto

---
+++ Seção 05 - Estruturas Repetitivas/48. Exercícios propostos PARTE 2 - problemas com estrutura enquanto/uri1114/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int senha = scanner.nextInt();
+        while (senha != 2002){
+            System.out.println("Senha Invalida");
+            senha = scanner.nextInt();
+        }
+        System.out.println("Acesso Permitido");
+        scanner.close();
+    }
+}
